88b8d304231cba6b5145c16cb4de6c8965e76c74
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-07 17:55:17 +0900

    インデックスの変数名をわかりやすいように修正

--- cafe.rb
+++ cafe.rb
@@ -26,10 +26,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_index = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_index = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drink_index][:price].to_i + FOODS[food_index][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
